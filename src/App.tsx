6646743faf712ce1ae92f059cdfe97251fe748ac
Giovanni Martin <71309186+giolmartin@users.noreply.github.com>
2023-04-28 18:30:15 -0600

    gh-pages config

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <ThemeProvider>
       <GlobalStyles />
-      <Router basename='https://giolmartin.github.io/portfolio'>
+      <Router basename={process.env.PUBLIC_URL}>
         {/* <Navbar /> */}
         <Routes>
           <Route path='/' element={<HomePage />} />
